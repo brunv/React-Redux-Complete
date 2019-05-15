cd6d19134b68ff0aed15baad78632689165cd03a
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-05-15 19:17:00 -0300

    add details of jsx vs html

--- 01-diving-into-react/src/index.js
+++ 01-diving-into-react/src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hey there!</div>;
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'orange', color: 'red'}}>Submit</button>
+        </div>);
 };
 
 // Take the react component and show it on the screen
