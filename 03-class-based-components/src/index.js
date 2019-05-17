55a516a6857af5e37dc96ae03659f14ace263673
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-05-17 17:08:24 -0300

    add more examples about state

--- 03-class-based-components/src/index.js
+++ 03-class-based-components/src/index.js
@@ -5,24 +5,33 @@ class App extends React.Component {
     constructor(props) {
         super(props);   // Super is a reference to the parent's constructor function
 
-        // ! THIS IS THE ONLY TIME we do direct assignment to this.state !
-        this.state = { lat: null };
+        // THIS IS THE ONLY TIME we do direct assignment to this.state !
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {                         // success callback
-                // Call setState():
+                // Call setState(): this will run asynchronously
                 this.setState({ lat: position.coords.latitude });
 
-                // ! Don't do this:
+                // Don't do this:
                 // this.state.lat = positions.coords.latitude
             },
-            (err) => console.log(err)               // failure callback
+            (err) => {                              // failure callback
+                this.setState({ errorMessage: err.message})
+            }
         );
     }
 
     // React says we have to define render!
     render () {
-        return <div>Latitude: {this.state.lat}</div>;
+        // Conditional rendering:
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+        return <div>Loading!</div>;
     }
 }
 
