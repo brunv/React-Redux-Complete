4f940a2c6fb4fef513254981bffed38456c9ff4a
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-05-16 22:50:17 -0300

    import and export component

--- 02-components/src/index.js
+++ 02-components/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div>
     );
 };
