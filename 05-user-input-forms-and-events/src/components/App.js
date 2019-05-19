f7d0cd3e9d44eada9c691395ff4c6b5430ea53d1
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-05-19 17:40:18 -0300

    add async await to the axios request

--- 05-user-input-forms-and-events/src/components/App.js
+++ 05-user-input-forms-and-events/src/components/App.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 0f9fa1f89e79e9b3ec5667e9484daf24ecb2204a8359856dd76cc80deee4cdaf'
             }
         });
+
+        console.log(response.data.results);
     }
 
     render() {
