ac88444ba567a59ca037d955adeedee2c78c50e2
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-05-24 13:32:40 -0300

    add styling to video components

--- 06-practice-application/src/components/VideoItem.js
+++ 06-practice-application/src/components/VideoItem.js
@@ -1,12 +1,19 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({ video }) => {
     // props was destructured
 
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} alt="" />
-            {video.snippet.title}
+        <div className="video-item item">
+            <img
+                src={video.snippet.thumbnails.medium.url}
+                alt=""
+                className="ui image"
+            />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
     );
 };
