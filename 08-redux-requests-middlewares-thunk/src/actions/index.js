63de1ef7c08ec6a9b2bc6c76543eb0fa574caf2c
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-06-21 00:06:08 -0300

    add alternate syntax with lodash

--- 08-redux-requests-middlewares-thunk/src/actions/index.js
+++ 08-redux-requests-middlewares-thunk/src/actions/index.js
@@ -22,8 +22,15 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     // manually dispatch the action creator:
     await dispatch(fetchPosts());
 
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(id => dispatch(fetchUser(id)));
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+    // same thing but using Lodash:
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value(); // executes all the steps above
 };
 
 export const fetchPosts = () => async (dispatch) => {
