c4874ec22dd93d63a63bc3a1e44842699355e868
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-06-24 12:18:02 -0300

    create a functional form

--- 11-forms-redux-form/client/src/components/streams/StreamCreate.js
+++ 11-forms-redux-form/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+// Remember: Field is capitalized because it is a React Component
+// and reduxForm is not because it is a function!
 
-export default StreamCreate;
+class StreamCreate extends React.Component {
+    renderInput({ input, label }) {
+        // console.log(formProps);
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
+    }
+
+    render() {
+        // console.log(this.props);
+        return (
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
