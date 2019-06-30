8214c7e7c1b901c115dd60f3b5f38c0396835e60
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-06-30 18:59:50 -0300

    passing id through url

--- 12-rest-based-apps/client/src/components/streams/StreamEdit.js
+++ 12-rest-based-apps/client/src/components/streams/StreamEdit.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
+    // We only have access to these props specifically because the streamEdit
+    // component inside of our App.js file is being rendered by a <Route>
+    // component, so react-router-dom is going to add in a bunch of different
+    // props to streamEdit when it gets rendered onto the screen.
+    // console.log(props);
+
     return <div>StreamEdit</div>;
 };
 
