cbb4cc7c07fc35fcc33f9c503193d90bbc09931c
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-06-30 14:53:50 -0300

    add all restful action creators

--- 12-rest-based-apps/client/src/actions/types.js
+++ 12-rest-based-apps/client/src/actions/types.js
@@ -1,7 +1,12 @@
 /**
  * This makes it easier to indentify errors of mispelling action
- * names anywhere in the application
+ * names anywhere in the application.
  */
 
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_STREAM';
+export const FETCH_STREAM = 'FETCH_STREAM';
+export const FETCH_STREAMS = 'FETCH_STREAMS';
+export const DELETE_STREAM = 'DELETE_STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
