cdd74436b939dde1f2d191d382bb527e40fcc58d
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-01 17:32:40 -0300

    implements delete stream

--- 13-react-portals/client/src/components/streams/StreamDelete.js
+++ 13-react-portals/client/src/components/streams/StreamDelete.js
@@ -1,28 +1,54 @@
 import React from 'react';
-import Modal from '../Modal'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
 
-    // We cannot remove the div that wraps up the two buttons because
-    // we are no allowed to assign multiple JSX elements to a single 
-    // variable. To do this we have to use React Fragment.
-    const actions = (
-        <React.Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>
-    );
+    componentDidMount() {
+        // console.log(this.props);
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-    return (
-        <div>
-            Stream StreamDelete
+    renderActions() {
+        const id = this.props.match.params.id;
+        // We cannot remove the div that wraps up the two buttons because
+        // we are no allowed to assign multiple JSX elements to a single 
+        // variable. To do this we have to use React Fragment.
+        return (
+            <React.Fragment>
+                <button onClick={() => this.props.deleteStream(id)} className="ui button negative">Delete</button>
+                <Link to="/" className="ui button">Cancel</Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return 'Are you sure you want to delete this stream?';
+        }
+
+        return `Are you sure you want to delete the "${this.props.stream.title}" stream?`;
+    }
+
+    render() {
+        return (
             <Modal
                 title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')}
             />
-        </div>
-    );
+        );
+    }
+}
+
+// Just a remainder: to get some information from our redux store into
+// our component we must define a mapStateToProps function.
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
