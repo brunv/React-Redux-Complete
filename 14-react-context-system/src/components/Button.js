9475f58327be968b521471ef6ff4a20d1794f694
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-06 17:21:07 -0300

    add context to the components

--- 14-react-context-system/src/components/Button.js
+++ 14-react-context-system/src/components/Button.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
 
+    static contextType = LanguageContext;
+
+    /**
+     * When we make use of the 'static' keyword we are adding
+     * a property directly to the class itself. 
+     */
+
     render() {
+        console.log(this.context);
+        const text = this.context === 'en-us' ? 'Submit' : 'Enviar';
         return (
             <button className="ui button primary">
-                Submit
+                {text}
             </button>
         );
     }
--- 14-react-context-system/src/components/Field.js
+++ 14-react-context-system/src/components/Field.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+    static contextType = LanguageContext;
 
     render() {
+        const text = this.context === 'en-us' ? 'Name' : 'Nome'
         return (
             <div className="ui field">
-                <label>Name</label>
+                <label>{text}</label>
                 <input />
             </div>
         );
