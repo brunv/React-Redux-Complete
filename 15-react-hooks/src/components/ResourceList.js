a4738d8dd99209e5ec408eca66b464cebbeebb8c
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-09 13:59:26 -0300

    another way to use useEffect

--- 15-react-hooks/src/components/ResourceList.js
+++ 15-react-hooks/src/components/ResourceList.js
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
-
+const useResources = (resource) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+    useEffect(
+        () => {
+            (async resource => {
+                const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-        setResources(response.data);
-    };
+                setResources(response.data);
+            })(resource);
+        },
+        [resource]
+    );
 
-    useEffect(() => {
-        fetchResource(resource);
-    }, [resource])
+    return resources;
+};
+
+const ResourceList = ({ resource }) => {
+    const resources = useResources(resource);
 
     return (
-        <div>
-            {resources.length}
-        </div>
+        <ul>
+            {resources.map(record => <li key={record.id}>{record.title}</li>)}
+        </ul>
     );
-}
+};
 
 export default ResourceList;
