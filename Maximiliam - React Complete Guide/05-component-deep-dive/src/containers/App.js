6ce22620a3ebb778d3549668106cf667f7e9419d
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-07-18 20:34:22 -0300

    updating state depending on the previous

--- Maximiliam - React Complete Guide/05-component-deep-dive/src/containers/App.js
+++ Maximiliam - React Complete Guide/05-component-deep-dive/src/containers/App.js
@@ -22,7 +22,8 @@ class App extends Component {
         ],
         otherState: 'some other value',
         showPersons: false,
-        showCockpit: true
+        showCockpit: true,
+        changeCounter: 0
     }
 
     static getDerivedStateFromProps(props, state) {
@@ -66,7 +67,14 @@ class App extends Component {
         const persons = [...this.state.persons];
         persons[personIndex] = person;
 
-        this.setState({ persons: persons });
+        this.setState((prevState, props) => {
+            // Using setState this way will garantee that React use
+            // the previous state as you wourld expect it fot this update
+            return ({
+                persons: persons,
+                changeCounter: prevState.changeCounter + 1
+            });
+        });
     }
 
     deletePersonHandler = (personIndex) => {
