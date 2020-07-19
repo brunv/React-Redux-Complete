4b76f04899fa0553fa662c3edd57909133370389
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-07-18 21:16:18 -0300

    using refs

--- Maximiliam - React Complete Guide/05-component-deep-dive/src/components/Persons/Person/Person.js
+++ Maximiliam - React Complete Guide/05-component-deep-dive/src/components/Persons/Person/Person.js
@@ -20,6 +20,16 @@ const StyledDiv = styled.div`
 `;
 
 class Person extends React.Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -27,7 +37,12 @@ class Person extends React.Component {
             <React.Fragment>
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p> {this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    // ref={(inputEl) => { this.inputElement = inputEl }} 
+                    ref={this.inputElementRef}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name} />
             </React.Fragment>
             // </div >
         )
