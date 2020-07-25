aff0a190a51f1eae783b43e259cd1560346f6737
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-07-24 23:24:17 -0300

    improve order summary

--- Maximiliam - React Complete Guide/06-burger-builder-basic/src/components/Burger/OrderSummary/OrderSummary.js
+++ Maximiliam - React Complete Guide/06-burger-builder-basic/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button/Button';
 
 const OrderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -17,7 +18,10 @@ const OrderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </React.Fragment>
     );
 };
