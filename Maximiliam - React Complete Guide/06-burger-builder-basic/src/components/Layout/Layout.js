4b8a5cf924a1dcd2006b2974212dd41410d4ff7a
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-07-25 14:43:07 -0300

    add complete navbar

--- Maximiliam - React Complete Guide/06-burger-builder-basic/src/components/Layout/Layout.js
+++ Maximiliam - React Complete Guide/06-burger-builder-basic/src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
-const Layout = (props) => {
-    return (
-        <React.Fragment>
-            <Toolbar />
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </React.Fragment>
-    );
-};
+class Layout extends React.Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState(prevState => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </React.Fragment >
+        );
+    }
+}
 
 export default Layout;
