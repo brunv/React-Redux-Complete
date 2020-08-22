79e8499219c190154d52ea8e178a998d59da1066
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-08-22 15:40:50 -0300

    sending post request to firebase

--- Maximiliam - React Complete Guide/08-bb-accessing-server/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Maximiliam - React Complete Guide/08-bb-accessing-server/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 1.5,
@@ -96,7 +97,25 @@ class BurgerBuilder extends React.Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingrediets: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Bruno Vieira',
+                address: {
+                    street: 'test street 1',
+                    zipCode: '1234134',
+                    country: 'Brazil'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
