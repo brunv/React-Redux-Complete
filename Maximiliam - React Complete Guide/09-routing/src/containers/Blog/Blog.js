ed90087d05c89a00b44d66bf40c60ca7f4a83277
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-08-25 16:54:31 -0300

    loading routes lazily

--- Maximiliam - React Complete Guide/09-routing/src/containers/Blog/Blog.js
+++ Maximiliam - React Complete Guide/09-routing/src/containers/Blog/Blog.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 import './Blog.css';
+
+const AsyncNewPost = asyncComponent(() => {
+    // This is a special syntax, the dynamic import syntax, which means whatever
+    // comes between the parentheses here is only imported when the arrow function
+    // is executed only when we render AsyncNewPost to the screen.
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        authenticated: false
+        authenticated: true
     }
 
     render() {
@@ -34,7 +42,7 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
                 <Switch>
-                    {this.state.authenticated ? <Route path="/new-post" exact component={NewPost} /> : null}
+                    {this.state.authenticated ? <Route path="/new-post" exact component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not found.</h1>} />
                     {/* <Redirect from="/" to="/posts" /> */}
