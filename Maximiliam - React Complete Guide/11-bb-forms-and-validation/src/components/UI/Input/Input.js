ed2d0e12a5f3b146c34ea1e81febf46e1bc93238
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-08-31 18:51:26 -0300

    dynamically create inputs based on js config

--- Maximiliam - React Complete Guide/11-bb-forms-and-validation/src/components/UI/Input/Input.js
+++ Maximiliam - React Complete Guide/11-bb-forms-and-validation/src/components/UI/Input/Input.js
@@ -5,16 +5,39 @@ import classes from './Input.module.css';
 const input = (props) => {
     let inputElement = null;
 
-    const { inputType, ...rest } = props;
-    switch (inputType) {
+    const { elementType, elementConfig, value, ...rest } = props;
+
+    switch (elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...rest} />;
+            inputElement = <input
+                className={classes.InputElement}
+                {...elementConfig}
+                value={value} />;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...rest} />;
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...elementConfig}
+                value={value} />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={value}>
+                    {elementConfig.options.map(opt => (
+                        <option key={opt.value} value={opt.value}>
+                            {opt.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...rest} />;
+            inputElement = <input
+                className={classes.InputElement}
+                {...elementConfig}
+                value={value} />;
     }
 
     return (
--- Maximiliam - React Complete Guide/11-bb-forms-and-validation/src/containers/Checkout/ContactData/ContactData.js
+++ Maximiliam - React Complete Guide/11-bb-forms-and-validation/src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends React.Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' },
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -48,13 +94,26 @@ class ContactData extends React.Component {
     }
 
     render() {
+        const formElementsArray = [];
+
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+        console.log(formElementsArray);
 
         let form = (
             <form>
-                <Input inputType="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputType="input" type="email" name="email" placeholder="Your Mail" />
-                <Input inputType="input" type="text" name="street" placeholder="Street" />
-                <Input inputType="input" type="text" name="postal" placeholder="Postal Code" />
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
