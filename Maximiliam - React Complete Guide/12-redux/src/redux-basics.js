30765f230f994397b8cdde7aefdd1967be9daa3e
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-09-15 19:39:56 -0300

    set up standard redux

---
+++ Maximiliam - React Complete Guide/12-redux/src/redux-basics.js
@@ -0,0 +1,24 @@
+// NodeJS import syntax:
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    return state;
+};
+
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+
+// Dispatching Action
+
+
+
+//Subscription
