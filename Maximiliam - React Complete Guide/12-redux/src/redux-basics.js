68eeb12e90dfdccae826d11f48e0cc3e87df190d
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-09-15 19:47:21 -0300

    use action dispatcher with standard redux

--- Maximiliam - React Complete Guide/12-redux/src/redux-basics.js
+++ Maximiliam - React Complete Guide/12-redux/src/redux-basics.js
@@ -8,7 +8,20 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case 'INC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'ADD_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        default:
+            return state;
+    }
 };
 
 
@@ -18,7 +31,9 @@ console.log(store.getState());
 
 
 // Dispatching Action
-
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
 
 
 //Subscription
