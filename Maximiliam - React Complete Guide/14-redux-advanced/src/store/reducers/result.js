69c96bec1f6750e2f314aa394cd7d32e07791cb6
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-09-27 17:11:48 -0300

    leaner switch case

--- Maximiliam - React Complete Guide/14-redux-advanced/src/store/reducers/result.js
+++ Maximiliam - React Complete Guide/14-redux-advanced/src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultId);
+    return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
@@ -12,12 +17,7 @@ const reducer = (state = initialState, action) => {
             return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) });
 
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results]; // copied the results in state
-            // newArray.splice(id, 1);              // removed the item by id (position)
-
-            const updatedArray = state.results.filter(result => result.id !== action.resultId);
-            return updateObject(state, { results: updatedArray });
+            return deleteResult(state, action);
 
         default:
             return state;
