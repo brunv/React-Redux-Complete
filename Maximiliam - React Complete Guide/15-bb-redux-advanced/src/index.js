550e147d974d6ddb1c36626ebbb1951f6d4ce74b
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-10-05 16:27:46 -0300

    set up redux devtools in bb project

--- Maximiliam - React Complete Guide/15-bb-redux-advanced/src/index.js
+++ Maximiliam - React Complete Guide/15-bb-redux-advanced/src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import App from './App';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <React.StrictMode>
