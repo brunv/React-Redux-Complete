2d7265c2d0c3986394fc0e579acd5cb9ec045a82
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-10-06 17:09:59 -0300

    add new order action

--- Maximiliam - React Complete Guide/15-bb-redux-advanced/src/containers/Checkout/ContactData/ContactData.js
+++ Maximiliam - React Complete Guide/15-bb-redux-advanced/src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends React.Component {
                 valid: true
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = (e) => {
@@ -193,7 +192,7 @@ class ContactData extends React.Component {
             </form>
         );
 
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />;
         }
 
@@ -209,14 +208,15 @@ class ContactData extends React.Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     };
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
