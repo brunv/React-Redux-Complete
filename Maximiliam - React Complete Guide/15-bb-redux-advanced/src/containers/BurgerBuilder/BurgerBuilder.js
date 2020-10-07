611ce2d1f9e67ebe1f81d6d44483232fc5ddfc14
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-10-07 16:55:35 -0300

    combine reducers

--- Maximiliam - React Complete Guide/15-bb-redux-advanced/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Maximiliam - React Complete Guide/15-bb-redux-advanced/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -105,11 +105,12 @@ class BurgerBuilder extends React.Component {
     }
 }
 
+// mapping state from combined reducers:
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        error: state.error
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        error: state.burgerBuilder.error
     };
 };
 
