19511d2bc2573a20ed7635f299f8f6060a4ae1e7
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-11-18 21:53:29 -0300

    add timing for enter and exit

--- Maximiliam - React Complete Guide/19-animations-in-react/src/components/Modal/Modal.js
+++ Maximiliam - React Complete Guide/19-animations-in-react/src/components/Modal/Modal.js
@@ -3,14 +3,19 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+};
+
 const modal = (props) => {
 
     return (
         <Transition
-            in={props.show}
-            timeout={300}
+            mountOnEnter
             unmountOnExit
-            mountOnEnter>
+            in={props.show}
+            timeout={animationTiming}>
             {state => {
                 const cssClasses = [
                     "Modal",
