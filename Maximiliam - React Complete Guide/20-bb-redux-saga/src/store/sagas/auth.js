7f5ab72ae51eaaa6375a0e2f0f0331099cffbc91
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-11-21 17:15:03 -0300

    add redux and create first saga

---
+++ Maximiliam - React Complete Guide/20-bb-redux-saga/src/store/sagas/auth.js
@@ -0,0 +1,15 @@
+import { put } from "redux-saga/effects";
+
+import * as actionTypes from '../actions/actionTypes';
+
+// the star turn this function into a "generator". Generators are nex gen JS features which are
+// functions that can be executed incremmentally.
+function* logout(action) {
+    yield localStorage.removeItem('token');
+    yield localStorage.removeItem('userId');
+    yield localStorage.removeItem('expirationDate');
+
+    yield put({
+        type: actionTypes.AUTH_LOGOUT
+    });
+}
