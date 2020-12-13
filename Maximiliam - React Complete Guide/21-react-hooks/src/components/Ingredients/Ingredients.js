76c195877f3165a4ca6ae704f6fd3c0eef5ab46b
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-12-13 13:57:35 -0300

    add useCallback() hook

--- Maximiliam - React Complete Guide/21-react-hooks/src/components/Ingredients/Ingredients.js
+++ Maximiliam - React Complete Guide/21-react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
+  /**
+   * All the functions defined inside of the component funciont are created
+   * again. So be careful and avoid creating an infinite loop.
+   */
 
   const [ingredients, setIngredients] = useState([]);
 
@@ -13,26 +17,33 @@ const Ingredients = () => {
    * component update (re-render) if no second argument is passed, acting
    * like the componentDidUpdate().
    */
+  // useEffect(() => {
+  //   fetch('https://react-hooks-cdf8b-default-rtdb.firebaseio.com/ingredients.json')
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       const loadedIngredients = [];
+  //       for (const key in responseData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount
+  //         });
+  //       }
+  //       setIngredients(loadedIngredients);
+  //     })
+  /**
+   * Used like this (with [] as a second argument), it acts like the
+   * componentDidMount(), which means that it runs only once (after the
+   * first render).
+   */
+  // }, []);
+
+  /**
+   * Here's an example of useEffect() with specific dependecies declared.
+   */
   useEffect(() => {
-    fetch('https://react-hooks-cdf8b-default-rtdb.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setIngredients(loadedIngredients);
-      })
-    /**
-     * Used like this (with [] as a second argument), it acts like the
-     * componentDidMount(), which means that it runs only once (after the
-     * first render).
-     */
-  }, []);
+    console.log('rendering ingredients...', ingredients);
+  }, [ingredients]);
 
   const addIngredientHandler = (ingredient) => {
     fetch('https://react-hooks-cdf8b-default-rtdb.firebaseio.com/ingredients.json', {
@@ -59,12 +70,23 @@ const Ingredients = () => {
     ));
   }
 
+  /**
+   * useCallback() hook receives a callback and and array as arguments. The hook
+   * will return a memoized version of the callback, that only changes if the
+   * dependencies changes.
+   * It basically caches the callback function so it survives rerun cycles and
+   * therefore the functiona is not recreated.
+   */
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setIngredients(filteredIngredients);
+  }, []);
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
