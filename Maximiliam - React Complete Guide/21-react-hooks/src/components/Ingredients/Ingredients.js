98e2a4bfbc407d1838233d564a2afa31d45ec7e6
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-12-13 10:31:15 -0300

    add useEffect hook

--- Maximiliam - React Complete Guide/21-react-hooks/src/components/Ingredients/Ingredients.js
+++ Maximiliam - React Complete Guide/21-react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,17 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
+
   const [ingredients, setIngredients] = useState([]);
 
+  /**
+   * This hook handles Side Effects. It gets executed right after every
+   * component update (re-render) if no second argument is passed, acting
+   * like the componentDidUpdate().
+   */
+  useEffect(() => {
+    fetch('https://react-hooks-cdf8b-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setIngredients(loadedIngredients);
+      })
+    /**
+     * Used like this (with [] as a second argument), it acts like the
+     * componentDidMount(), which means that it runs only once (after the
+     * first render).
+     */
+  }, []);
+
   const addIngredientHandler = (ingredient) => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.floor(Math.random() * 100).toString(), ...ingredient }
-    ]);
+    fetch('https://react-hooks-cdf8b-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    });
+
   }
 
   const removeIngredientHandler = (ingredientId) => {
