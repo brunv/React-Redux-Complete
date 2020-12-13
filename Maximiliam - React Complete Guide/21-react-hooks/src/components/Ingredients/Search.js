276c7775a33a881893334a3f67e2a62bac8d8e9a
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2020-12-13 16:25:53 -0300

    cleaning up with useEffect() hook

--- Maximiliam - React Complete Guide/21-react-hooks/src/components/Ingredients/Search.js
+++ Maximiliam - React Complete Guide/21-react-hooks/src/components/Ingredients/Search.js
@@ -7,12 +7,13 @@ const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   /**
-   * useRef()
+   * useRef() returns a mutable ref object, accessed in the DOM. This object will 
+   * survive the whole component lifecycle.
    */
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length ? `?orderBy="title"&equalTo="${enteredFilter}"` : '';
         fetch('https://react-hooks-cdf8b-default-rtdb.firebaseio.com/ingredients.json' + query)
@@ -30,6 +31,15 @@ const Search = React.memo(props => {
           })
       }
     }, 500);
+    /**
+     * useEffect() can return a function for clean up. In this case we have to
+     * clean up the older timeouts that shouldn't be used anymore.
+     * If we had no dependencies ([]) the clean up function would run when the
+     * component gets unmounted.
+     */
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
